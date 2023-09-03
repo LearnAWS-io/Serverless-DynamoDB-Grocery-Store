b8a6f3984e8e37e199b4b75a9ef16ae04b7c217c
Shivam <singhshivamkr@gmail.com>
2023-09-03 10:59:12 +0530

    Initial commit

---
+++ test/grocery-store-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GroceryStoreBackend from '../lib/grocery-store-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/grocery-store-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GroceryStoreBackend.GroceryStoreBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
