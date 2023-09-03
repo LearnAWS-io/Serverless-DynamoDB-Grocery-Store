8386b1b7568d069e2187aea250d3d0cd4941a5fd
Shivam <singhshivamkr@gmail.com>
2023-09-04 00:22:20 +0530

    :sparkles: make schmea convert to uppercase

--- schemas/add-grocery.ts
+++ schemas/add-grocery.ts
@@ -2,9 +2,9 @@ import v from "@vinejs/vine";
 
 const AddGroceryObj = v.array(
   v.object({
-    category: v.string(),
-    name: v.string(),
-    type: v.string(),
+    category: v.string().toUpperCase(),
+    name: v.string().toUpperCase(),
+    type: v.string().toUpperCase(),
     stocks: v.number(),
     price: v.object({
       inr: v.number(),
