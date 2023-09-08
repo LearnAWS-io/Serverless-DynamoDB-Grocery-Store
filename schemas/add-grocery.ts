a8e79828111fcfaa48ddce846ec9a54e6032ecaa
Shivam <singhshivamkr@gmail.com>
2023-09-08 13:44:02 +0530

    :sparkles: add max length of 25 on body schema

--- schemas/add-grocery.ts
+++ schemas/add-grocery.ts
@@ -1,16 +1,18 @@
 import v from "@vinejs/vine";
 
-const AddGroceryObj = v.array(
-  v.object({
-    category: v.string().toUpperCase(),
-    name: v.string().toUpperCase(),
-    type: v.string().toUpperCase(),
-    stocks: v.number(),
-    price: v.object({
-      inr: v.number(),
-      usd: v.number(),
+const AddGroceryObj = v
+  .array(
+    v.object({
+      category: v.string().toUpperCase(),
+      name: v.string().toUpperCase(),
+      type: v.string().toUpperCase(),
+      stocks: v.number(),
+      price: v.object({
+        inr: v.number(),
+        usd: v.number(),
+      }),
     }),
-  }),
-);
+  )
+  .maxLength(25);
 
 export const addGroceryObjValidator = v.compile(AddGroceryObj);
