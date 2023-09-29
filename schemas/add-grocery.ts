5764fbbd4d553a486400f5f8a20aafde393843ca
Shivam <singhshivamkr@gmail.com>
2023-09-29 16:39:58 +0530

    :sparkles: update schema for separate Grocery obj

--- schemas/add-grocery.ts
+++ schemas/add-grocery.ts
@@ -1,18 +1,17 @@
 import v from "@vinejs/vine";
 
-const AddGroceryObj = v
-  .array(
-    v.object({
-      category: v.string().toUpperCase(),
-      name: v.string().toUpperCase(),
-      type: v.string().toUpperCase(),
-      stocks: v.number(),
-      price: v.object({
-        inr: v.number(),
-        usd: v.number(),
-      }),
-    }),
-  )
-  .maxLength(25);
+export const GroceryItemObj = v.object({
+  category: v.string().toUpperCase(),
+  name: v.string().toUpperCase(),
+  type: v.string().toUpperCase(),
+  stocks: v.number(),
+  price: v.object({
+    inr: v.number(),
+    usd: v.number(),
+  }),
+});
+
+const AddGroceryObj = v.array(GroceryItemObj).maxLength(25);
 
 export const addGroceryObjValidator = v.compile(AddGroceryObj);
+export const groceryObjItemValidator = v.compile(GroceryItemObj);
