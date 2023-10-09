4b635594a610e7a2021b355f0aa702dcaeaffff7
Shivam <singhshivamkr@gmail.com>
2023-10-09 17:26:18 +0530

    :bug: fix add grocery API to return ids

--- lambdas/add-grocery-items-fn.ts
+++ lambdas/add-grocery-items-fn.ts
@@ -1,16 +1,16 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-import { BatchWriteItemCommand } from "@aws-sdk/client-dynamodb";
+import { BatchWriteItemCommand, WriteRequest } from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 import { addGroceryObjValidator } from "../schemas/add-grocery";
 import { errors } from "@vinejs/vine";
 import { dbClient } from "./db-client";
 import { defKSUID32 } from "@thi.ng/ksuid";
 import { BASE16 } from "@thi.ng/base-n";
+import { ResponseHeaders } from "./constants";
 
 const ksuid = defKSUID32({ base: BASE16, bytes: 8 });
 
-export const handler: APIGatewayProxyHandlerV2<
-  Record<string, unknown>
-> = async (e) => {
+export const handler: APIGatewayProxyHandlerV2<string> = async (e) => {
   const body = e.body;
   const TableName = process.env.TableName;
   try {
@@ -23,49 +23,59 @@ export const handler: APIGatewayProxyHandlerV2<
     const json = JSON.parse(body);
     const items = await addGroceryObjValidator.validate(json);
 
+    const putRequestItems: WriteRequest[] = items.map((item) => {
+      const { type, name, price, stocks, category } = item;
+      const id = ksuid.next();
+      const reqItem = {
+        PK: id,
+        SK: id,
+        GSI1PK: category,
+        GSI1SK: `${name}#${type}`,
+        stocks: stocks,
+        price: {
+          inr: price.inr,
+          usd: price.usd,
+        },
+      };
+
+      return {
+        PutRequest: {
+          Item: marshall(reqItem),
+        },
+      };
+    });
+
     const batchPutItemCmd = new BatchWriteItemCommand({
       RequestItems: {
-        [TableName]: items.map((item) => {
-          const { type, name, price, stocks, category } = item;
-          const id = ksuid.next();
-          return {
-            PutRequest: {
-              Item: {
-                PK: { S: id },
-                SK: { S: id },
-                GSI1PK: { S: category },
-                GSI1SK: { S: `${name}#${type}` },
-                stocks: { N: stocks.toString() },
-                price: {
-                  M: {
-                    inr: { N: price.inr.toString() },
-                    usd: { N: price.usd.toString() },
-                  },
-                },
-              },
-            },
-          };
-        }),
+        [TableName]: putRequestItems,
       },
     });
 
-    const res = await dbClient.send(batchPutItemCmd);
+    await dbClient.send(batchPutItemCmd);
+    const reqIds = putRequestItems.map((req) => req.PutRequest?.Item?.PK.S);
     return {
-      body: res,
+      headers: ResponseHeaders,
+      statusCode: 200,
+      body: JSON.stringify({
+        items: reqIds,
+      }),
     };
   } catch (err) {
+    console.log(err);
     if (err instanceof errors.E_VALIDATION_ERROR) {
       // array created by SimpleErrorReporter
       return {
         statusCode: 400,
-        body: err,
+        headers: ResponseHeaders,
+        body: JSON.stringify({ error: err }),
       };
     }
     return {
       statusCode: 400,
-      body: {
+      headers: ResponseHeaders,
+      body: JSON.stringify({
         error: err instanceof Error ? err.message : err,
-      },
+      }),
     };
   }
 };
---
+++ lambdas/constants.ts
@@ -0,0 +1,3 @@
+export const ResponseHeaders = {
+  "Content-Type": "application/json",
+};
